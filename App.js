846418d3fcb2832ab4eac2ba57ee3d46a106b608
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-05-24 11:50:28 -0700

    Leared about the basics of state

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 // state'full' component
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {txt: 'this is the state txt'}
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
   render(){
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default prop.txt value'
-}
-
-ReactDOM.render(<App cat={4} /*txt="this is the props value"*/ />, document.getElementById('app'));
-
-// export default App
+export default App
